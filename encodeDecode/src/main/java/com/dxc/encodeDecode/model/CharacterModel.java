abdf14beb9c6f2d80d4b90af76a7b8cf5307fb7a
abigail-143 <abigailtoh1403@gmail.com>
2023-11-04 14:36:09 +0800

    create characterlist model

---
+++ encodeDecode/src/main/java/com/dxc/encodeDecode/model/CharacterModel.java
@@ -0,0 +1,36 @@
+package com.dxc.encodeDecode.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "character_list")
+public class CharacterModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer index;
+    private Character character;
+
+    public CharacterModel(Integer index, Character character) {
+        this.index = index;
+        this.character = character;
+    }
+
+    public CharacterModel() {
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    private void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Character character) {
+        this.character = character;
+    }
+}
