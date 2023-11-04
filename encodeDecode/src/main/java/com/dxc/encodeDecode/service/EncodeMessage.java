85e258a271d697ddedc2eecd1792655e018a50f9
abigail-143 <abigailtoh1403@gmail.com>
2023-11-04 19:16:43 +0800

    created some methods

--- encodeDecode/src/main/java/com/dxc/encodeDecode/service/EncodeMessage.java
+++ encodeDecode/src/main/java/com/dxc/encodeDecode/service/EncodeMessage.java
@@ -1,25 +1,114 @@
 package com.dxc.encodeDecode.service;
 
+import com.dxc.encodeDecode.model.CharacterModel;
+import com.dxc.encodeDecode.repository.CharacterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @Service
 public class EncodeMessage {
-    private String message;
-    private Integer shift;
-    private String encodedMessage;
-    private String response;
 
-    public EncodeMessage(String message, Integer shift, String encodedMessage, String response) {
-        this.message = message;
-        this.shift = shift;
-        this.encodedMessage = encodedMessage;
-        this.response = response;
+    @Autowired
+    private CharacterModel characterModel;
+    public EncodeMessage(CharacterModel characterModel) {
+        this.characterModel = characterModel;
+    }
+    @Autowired
+    private CharacterRepository characterRepository;
+    public EncodeMessage(CharacterRepository characterRepository) {
+        this.characterRepository = characterRepository;
     }
 
-    public EncodeMessage() {
+    private Long totalCount;
+    public EncodeMessage(Long totalCount) {
+        this.totalCount = totalCount;
     }
 
-    public EncodeMessage(String response) {
-        this.response = response;
+    // get the shift key first
+    public int getShiftKey(String password) {
+        try {
+            // check password input
+            int shiftKey = password.length();
+            if (shiftKey > totalCount) {
+                System.out.println("pw length > total count")
+                int newShiftKey = Math.toIntExact(shiftKey % totalCount);
+                if (newShiftKey == 0) {
+                    System.out.println("% is 0")
+                    shiftKey = 1;
+                    System.out.println(shiftKey);
+                    return shiftKey;
+                } else {
+                    System.out.println("% is not 0");
+                    shiftKey = newShiftKey;
+                    System.out.println(shiftKey);
+                    return shiftKey;
+
+                }
+            }
+            if (shiftKey == totalCount) {
+                System.out.println("pw length = total count");
+                shiftKey = 1;
+                System.out.println(shiftKey);
+                return shiftKey;
+            }
+            return shiftKey;
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
     }
+
+    // get message indexes
+    public int[] messageIndex(String message) {
+       char[] messageArray = message.toCharArray();
+       int[] indexArray = new int[messageArray.length];
+       int length = messageArray.length;
+       for (int i = 0; i < length; i++) {
+            Optional<CharacterModel> character = characterRepository.findByCharacter(messageArray[i]);
+            if (character.isPresent()) {
+                CharacterModel existingChar = character.get();
+                int index = existingChar.getIndex();
+                indexArray[i] = index;
+            }
+
+       }
+       System.out.println(Arrays.toString(indexArray));
+       return indexArray;
+    }
+
+    // get shifted message indexes
+    public int[] newMessageIndex(int[] messageIndex, int shiftKey) {
+        int[] newMessageIndex = new int[messageIndex.length];
+        for (int i = 0; i < messageIndex.length; i++) {
+            int newIndex = messageIndex[i] + shiftKey;
+            if (newIndex > totalCount) {
+                newIndex = (int) (newIndex - totalCount);
+                newMessageIndex[i] = newIndex;
+            }
+            newMessageIndex[i] = newIndex;
+        }
+        System.out.println(Arrays.toString(newMessageIndex));
+        return newMessageIndex;
+    }
+
+    public String encodedMessage(int[] newMessageIndex) {
+        char[] newMessageArray = new char[newMessageIndex.length];
+        for (int i = 0; i < newMessageArray.length; i++) {
+            Optional<CharacterModel> index = characterRepository.findByIndex(newMessageIndex[i]);
+            if (index.isPresent()) {
+                CharacterModel existingIndex = index.get();
+                char character = existingIndex.getCharacter();
+                newMessageArray[i] = character;
+            } else {
+                System.out.println("Can't find index");
+                return index.toString();
+            }
+        }
+        return Arrays.toString(newMessageArray);
+    }
+
+
 }
