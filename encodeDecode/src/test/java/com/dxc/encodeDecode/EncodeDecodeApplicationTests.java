85286ad1a354912afa04fef8c1ce13e851943e29
abigail-143 <abigailtoh1403@gmail.com>
2023-11-04 14:36:01 +0800

    prep test files

---
+++ encodeDecode/src/test/java/com/dxc/encodeDecode/EncodeDecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.encodeDecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncodeDecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
